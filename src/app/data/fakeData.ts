ec56f2a83e562a0b14194c5d868f3ed825af7300
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-10-29 11:11:57 -0300

    feat: Content page and fake data pre setting

---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,14 @@
+export const fakeData = [
+  {
+    id: 1,
+    picture: 'aaa',
+    title: 'Nova Noticia',
+    description: 'lorem ipsun',
+  },
+  {
+    id: 2,
+    picture: 'aaa',
+    title: 'Nova Noticia',
+    description: 'lorem ipsun',
+  },
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { TitleComponent } from "../../components/title/title.component";
-import { BigCardComponent } from "../../components/big-card/big-card.component";
-import { SmallCardComponent } from "../../components/small-card/small-card.component";
-import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+import { TitleComponent } from '../../components/title/title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
+  imports: [
+    TitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuBarComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
